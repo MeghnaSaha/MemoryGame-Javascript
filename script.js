7231ace119e9e27662b7d3919903c5bce600f889
Glitch (hello-webpage) <none>
2020-05-09 15:42:16 +0000

    🗾🍦 Checkpoint ./style.css:16004622/83 ./script.js:16004622/9 ./index.html:16004622/354

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll(".memory-card");
 
 function flipCard(){
-  this.classList.toggle("flip");
+  this.classList.add("flip");
 }
 
 cards.forEach(card => card.addEventListener('click',flipCard));
