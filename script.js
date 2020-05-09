a5f7c50e01a5666c5bf34f7acc00ca92a64d0ebc
Glitch (hello-webpage) <none>
2020-05-09 15:53:17 +0000

    🍩🐁 Checkpoint ./script.js:16004622/642

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard(){
   this.classList.add("flip");
+  
+  if(!hasFlippedCard){
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+  }
+  else{
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+  }
+  
+  if(firstCard.dataset.framework === secondCard.dataset.framework){
+    //match
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click',flipCard);
+  }
+  else{
+    //not a match
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+  }
 }
 
 cards.forEach(card => card.addEventListener('click',flipCard));
